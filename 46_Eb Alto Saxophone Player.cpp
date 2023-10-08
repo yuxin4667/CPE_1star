47b9bacab53f7d21ea88f8c5848996348120f4f4
yuxin4667 <yuxin4667@gmail.com>
2023-10-08 15:09:14 +0800

    46_map版本

--- 46_Eb Alto Saxophone Player.cpp
+++ 46_Eb Alto Saxophone Player.cpp
@@ -1,95 +1,61 @@
 #include <iostream>
-#include <cstring>
+#include <map>
+#include <string>
+#include <vector>
 using namespace std;
 
-void press(char ch, int* finger)
-{
-	for(int j=0;j<11;j++)
-	{
-		finger[j]=0;
-	}
-	if('a'<=ch && 'g'>=ch)
-	{
-		switch (ch)
-		{
-			case 'c':
-				finger[10]=1;
-			case 'd':
-				finger[9]=1;
-			case 'e':
-				finger[8]=1;
-			case 'f':
-				finger[7]=1;
-			case 'g':
-				finger[4]=1;
-			case 'a':
-				finger[3]=1;
-			case 'b':
-				finger[2]=1;
-			default:
-				break;	
-		}
-	}
-	else if('A'<=ch && 'G'>=ch)
-	{
-		switch (ch)
-		{
-			case 'D':
-				finger[9]=1;
-			case 'E':
-				finger[8]=1;
-			case 'F':
-				finger[7]=1;
-			case 'G':
-				finger[4]=1;
-			case 'A':
-				finger[3]=1;
-			case 'B':
-				finger[1]=1;
-				finger[2]=1;
-				break;
-			case 'C':
-				finger[3]=1;
-				break;
-			default:
-				break;
-		}	
-	}
-}
 int main()
 {
-	int data;
-	cin >> data;
-	while(data--)
-	{
-		string str;
-		cin >> str;
-		
-		int finger[11]={0};
-		int f1[11]={0};
-		int f2[11]={0};
-		
-		for(int i=0;i<str.length();i++)
-		{
-			int ch=str[i];
-			press(ch, f2);
-			
-			for(int j=0;j<11;j++)
-			{
-				if(f2[j]-f1[j]==1)
-				finger[j]++;
-			}
-			
-			for(int j=0;j<11;j++)
-				f1[j]=f2[j];
-		}
-		for(int i=1;i<11;i++)
-		{
-			cout << finger[i];
-			if(i!=10)
-				cout << ' ';
-		}
-		cout << endl;
-	}
-	return 0;
+    map<char, vector<int>> mp;
+    mp['c'] = {0, 1, 1, 1, 0, 0, 1, 1, 1, 1};
+    mp['d'] = {0, 1, 1, 1, 0, 0, 1, 1, 1, 0};
+    mp['e'] = {0, 1, 1, 1, 0, 0, 1, 1, 0, 0};
+    mp['f'] = {0, 1, 1, 1, 0, 0, 1, 0, 0, 0};
+    mp['g'] = {0, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+    mp['a'] = {0, 1, 1, 0, 0, 0, 0, 0, 0, 0};
+    mp['b'] = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+    mp['C'] = {0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+    mp['D'] = {1, 1, 1, 1, 0, 0, 1, 1, 1, 0};
+    mp['E'] = {1, 1, 1, 1, 0, 0, 1, 1, 0, 0};
+    mp['F'] = {1, 1, 1, 1, 0, 0, 1, 0, 0, 0};
+    mp['G'] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+    mp['A'] = {1, 1, 1, 0, 0, 0, 0, 0, 0, 0};
+    mp['B'] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    int t;
+    string s;
+    cin >> t;
+    getline(cin, s); // clear buffer
+    while (t--)
+    {
+        getline(cin, s);
+        int count[10] = {0};
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (mp[s[i + 1]][j] - mp[s[i]][j] == 1)
+                {
+                    count[j]++;
+                }
+            }
+        }
+        for (int j = 0; j < 10; j++)
+        {
+            if (mp[s[0]][j] - 0 == 1)
+            {
+                count[j]++;
+            }
+        }
+
+        for (int j = 0; j < 10; j++)
+        {
+            cout << count[j];
+            if (j != 9)
+                cout << ' ';
+            else
+                cout << endl;
+        }
+    }
+    return 0;
 }
