dbe6eaaf74a97815a1899192cc1de0a3105f0010
yuxin4667 <yuxin4667@gmail.com>
2023-10-08 11:17:31 +0800

    45_vector版

--- 45_Die Game.cpp
+++ 45_Die Game.cpp
@@ -1,61 +1,55 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int n;
-	while(cin >> n && n!=0)
-	{
-		int WE[4]={3, 1, 4, 6};
-		int NS[4]={1, 5 ,6, 2};
-		while(n--)
-		{
-			string str;
-			cin >> str;
+    int n;
+    while (cin >> n && n != 0)
+    {
+        // int WE[4]={3, 1, 4, 6};
+        // int NS[4]={1, 5 ,6, 2};
+        vector<int> NS{1, 5, 6, 2};
+        vector<int> WE{1, 3, 6, 4};
+        while (n--)
+        {
+            string str;
+            cin >> str;
 
-			if(str.compare("north")==0)
-			{
-				int temp=NS[0];
-				for(int i=0;i<3;i++)
-					NS[i]=NS[i+1];
-				
-				NS[3]=temp;	
-				WE[1]=NS[0];
-				WE[3]=NS[2];
-			}
-			else if(str.compare("south")==0)
-			{
-				int temp=NS[3];
-				for(int i=3;i>0;i--)
-					NS[i]=NS[i-1];
-				
-				NS[0]=temp;	
-				WE[1]=NS[0];
-				WE[3]=NS[2];
-			}
-			else if(str.compare("east")==0)
-			{
-				int temp=WE[3];
-				for(int i=3;i>0;i--)
-					WE[i]=WE[i-1];
-				
-				WE[0]=temp;	
-				NS[0]=WE[1];
-				NS[2]=WE[3];	
-			}
-			else if(str.compare("west")==0)
-			{
-				int temp=WE[0];
-				for(int i=0;i<3;i++)
-					WE[i]=WE[i+1];
-				
-				WE[3]=temp;
-				NS[0]=WE[1];
-				NS[2]=WE[3];	
-			}
-		}
-		cout << NS[0] << endl;
-	}
-	return 0;
+            if (str.compare("north") == 0)
+            {
+                NS.push_back(NS[0]);
+                NS.erase(NS.begin());
+
+                WE[0] = NS[0];
+                WE[2] = NS[2];
+            }
+            else if (str.compare("south") == 0)
+            {
+                NS.insert(NS.begin(), NS[3]);
+                NS.pop_back();
+
+                WE[0] = NS[0];
+                WE[2] = NS[2];
+            }
+            else if (str.compare("east") == 0)
+            {
+                WE.push_back(WE[0]);
+                WE.erase(WE.begin());
+
+                NS[0] = WE[0];
+                NS[2] = WE[2];
+            }
+            else if (str.compare("west") == 0)
+            {
+                WE.insert(WE.begin(), WE[3]);
+                WE.pop_back();
+
+                NS[0] = WE[0];
+                NS[2] = WE[2];
+            }
+        }
+        cout << NS[0] << endl;
+    }
+    return 0;
 }
-
